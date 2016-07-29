142c0bdbce6a412524054a4650ffc3ff25c864cb
Fabian Damken <fabian.damken@gmx.de>
2016-07-29 11:52:32 +0200

    Added possibility to import other config files.
    * Added package configuration for import property.
    * Set default value tp '$import'
    
    This can be used to import other configuration files. This is
    useful if some of the config files contain secret information
    that shall not be populated into the VCS.

--- main.js
+++ main.js
@@ -2,28 +2,51 @@
 "use strict";
 
 var packageConfig = require(__dirname + "/package.config.json");
+var importProperty = packageConfig.importProperty;
 var fileName = packageConfig.defaultConfigFile;
 var args = require("optimist").argv;
 var fs = require("fs");
+var extend = require('extend');
 var path = require("path");
 
 if(args[packageConfig.commandLineSwitchName]) {
     fileName = args[packageConfig.commandLineSwitchName];
 }
-var rootDir = process.cwd();
 
-var configData = "";
-var filePath = path.normalize(rootDir) + "/" + fileName;
+var _readFile = function(filePath) {
+    var dirPath = path.dirname(filePath);
+    var result = {};
 
-try {
-    configData = fs.readFileSync(filePath, "utf-8");
-    module.exports = JSON.parse(configData);
-}
-catch(e) {
-    var msg = 'Unable to read or parse file "' + filePath + '". Exception caught: ' + e;
-    if(packageConfig.throwOnError) {
-        throw new Error(msg);
+    var config;
+    try {
+        config = fs.readFileSync(filePath, "utf-8");
+    } catch (e) {
+        var msg = 'Unable to read or parse file "' + path + '". Exception caught: ' + e;
+        if(packageConfig.throwOnError) {
+            throw new Error(msg);
+        }
+        console.error(msg);
+        return null;
     }
-    console.error(msg);
-    module.exports = null;
-}
+    extend(true, result, config);
+
+    if(result[importProperty]) {
+        var imports = (result[importProperty] instanceof Array ? result[importProperty] : [result[importProperty]]);
+        delete result[importProperty];
+        for(var i = 0; i < imports.length; i++) {
+            var importConfig = _readFile(path.join(dirPath, imports[i]));
+            if (!importConfig) {
+                // An error occurred whilst parsing the imported configuration. If
+                // throwOnError was set to true, an error was thrown. Otherwise, _readFile
+                // returned 'null', to check for this and return 'null' again.
+
+                return null;
+            }
+            extend(true, result, importConfig);
+        }
+    }
+
+    return result;
+};
+
+module.exports = _readFile(path.join(path.normalize(process.cwd()), fileName));
